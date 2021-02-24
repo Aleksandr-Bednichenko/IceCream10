0721fbbaf3325171b5c37108383533a3986e4f77
Aleksandr-Bednichenko <a0933093518@gmail.com>
2021-02-24 19:18:00 +0200

    Updates

--- jquery.bpopup.min.a836f41e.js
+++ jquery.bpopup.min.a836f41e.js
@@ -117,219 +117,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"../node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
-var bundleURL = null;
-
-function getBundleURLCached() {
-  if (!bundleURL) {
-    bundleURL = getBundleURL();
-  }
-
-  return bundleURL;
-}
-
-function getBundleURL() {
-  // Attempt to find the URL of the current script and use that as the base URL
-  try {
-    throw new Error();
-  } catch (err) {
-    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
-
-    if (matches) {
-      return getBaseURL(matches[0]);
-    }
-  }
-
-  return '/';
-}
-
-function getBaseURL(url) {
-  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)\/[^/]+$/, '$1') + '/';
-}
-
-exports.getBundleURL = getBundleURLCached;
-exports.getBaseURL = getBaseURL;
-},{}],"../node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
-var bundle = require('./bundle-url');
-
-function updateLink(link) {
-  var newLink = link.cloneNode();
-
-  newLink.onload = function () {
-    link.remove();
-  };
-
-  newLink.href = link.href.split('?')[0] + '?' + Date.now();
-  link.parentNode.insertBefore(newLink, link.nextSibling);
-}
-
-var cssTimeout = null;
-
-function reloadCSS() {
-  if (cssTimeout) {
-    return;
-  }
-
-  cssTimeout = setTimeout(function () {
-    var links = document.querySelectorAll('link[rel="stylesheet"]');
-
-    for (var i = 0; i < links.length; i++) {
-      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
-        updateLink(links[i]);
-      }
-    }
-
-    cssTimeout = null;
-  }, 50);
-}
-
-module.exports = reloadCSS;
-},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"sass/main.scss":[function(require,module,exports) {
-var reloadCSS = require('_css_loader');
-
-module.hot.dispose(reloadCSS);
-module.hot.accept(reloadCSS);
-},{"./..\\images\\products\\product1.png":[["product1.c41d1415.png","images/products/product1.png"],"images/products/product1.png"],"./..\\images\\products\\product1x2.png":[["product1x2.4bf99676.png","images/products/product1x2.png"],"images/products/product1x2.png"],"./..\\images\\products\\product2.png":[["product2.91372433.png","images/products/product2.png"],"images/products/product2.png"],"./..\\images\\products\\product2x2.png":[["product2x2.0b4b0a51.png","images/products/product2x2.png"],"images/products/product2x2.png"],"./..\\images\\products\\product3.png":[["product3.59e66af6.png","images/products/product3.png"],"images/products/product3.png"],"./..\\images\\products\\product3x2.png":[["product3x2.baa72e34.png","images/products/product3x2.png"],"images/products/product3x2.png"],"./..\\images\\products\\dots.svg":[["dots.08c47106.svg","images/products/dots.svg"],"images/products/dots.svg"],"./..\\images\\products\\arrow-right.png":[["arrow-right.43fe967f.png","images/products/arrow-right.png"],"images/products/arrow-right.png"],"./..\\images\\products\\arrow-rightx2.png":[["arrow-rightx2.4ba5e216.png","images/products/arrow-rightx2.png"],"images/products/arrow-rightx2.png"],"./..\\images\\advantages\\icon-milk.png":[["icon-milk.2f7ad634.png","images/advantages/icon-milk.png"],"images/advantages/icon-milk.png"],"./..\\images\\advantages\\icon-apples.png":[["icon-apples.7a6a7f24.png","images/advantages/icon-apples.png"],"images/advantages/icon-apples.png"],"./..\\images\\advantages\\icon-icecream.png":[["icon-icecream.f1c74b89.png","images/advantages/icon-icecream.png"],"images/advantages/icon-icecream.png"],"./..\\images\\advantages\\icon-milk@2x.png":[["icon-milk@2x.da0a9131.png","images/advantages/icon-milk@2x.png"],"images/advantages/icon-milk@2x.png"],"./..\\images\\advantages\\icon-apples@2x.png":[["icon-apples@2x.0f104d6a.png","images/advantages/icon-apples@2x.png"],"images/advantages/icon-apples@2x.png"],"./..\\images\\advantages\\icon-icecream@2x.png":[["icon-icecream@2x.f6e78263.png","images/advantages/icon-icecream@2x.png"],"images/advantages/icon-icecream@2x.png"],"./..\\images\\customers-section\\icon-semicolon.svg":[["icon-semicolon.23dfa828.svg","images/customers-section/icon-semicolon.svg"],"images/customers-section/icon-semicolon.svg"],"./..\\images\\customers-section\\icon-dots.svg":[["icon-dots.c97d14bb.svg","images/customers-section/icon-dots.svg"],"images/customers-section/icon-dots.svg"],"./..\\images\\desktop\\cards\\sectionbgcards.png":[["sectionbgcards.f7d3e6a5.png","images/desktop/cards/sectionbgcards.png"],"images/desktop/cards/sectionbgcards.png"],"./..\\images\\desktop\\cards\\sectionbgcards@2x.png":[["sectionbgcards@2x.072b8a0b.png","images/desktop/cards/sectionbgcards@2x.png"],"images/desktop/cards/sectionbgcards@2x.png"],"C:\\Users\\user\\Documents\\GitHub\\IceCream10\\src\\images\\about\\mobile\\sectionbg1.png":[["sectionbg1.e6dd4e96.png","images/about/mobile/sectionbg1.png"],"images/about/mobile/sectionbg1.png"],"C:\\Users\\user\\Documents\\GitHub\\IceCream10\\src\\images\\about\\mobile\\sectionbg1@2x.png":[["sectionbg1@2x.9b16eb57.png","images/about/mobile/sectionbg1@2x.png"],"images/about/mobile/sectionbg1@2x.png"],"C:\\Users\\user\\Documents\\GitHub\\IceCream10\\src\\images\\about\\desktop\\sectionbg1.png":[["sectionbg1.015412c1.png","images/about/desktop/sectionbg1.png"],"images/about/desktop/sectionbg1.png"],"C:\\Users\\user\\Documents\\GitHub\\IceCream10\\src\\images\\about\\desktop\\sectionbg1@2x.png":[["sectionbg1@2x.1c1dbe95.png","images/about/desktop/sectionbg1@2x.png"],"images/about/desktop/sectionbg1@2x.png"],"./..\\images\\check.svg":[["check.4b7c2681.svg","images/check.svg"],"images/check.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"js/burger.js":[function(require,module,exports) {
-(function () {
-  var menuBtnRef = document.querySelector("[data-menu-batton]");
-  var mobileMenuRef = document.querySelector("[data-menu]");
-  menuBtnRef.addEventListener("click", function () {
-    var expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-    menuBtnRef.classList.toggle("is-active");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
-    mobileMenuRef.classList.toggle("is-open");
-  });
-})();
-},{}],"js/slider.js":[function(require,module,exports) {
-// vars
-'use strict';
-
-var testim = document.getElementById("testim"),
-    testimDots = Array.prototype.slice.call(document.getElementById("testim-dots").children),
-    testimContent = Array.prototype.slice.call(document.getElementById("testim-content").children),
-    testimLeftArrow = document.getElementById("left-arrow"),
-    testimRightArrow = document.getElementById("right-arrow"),
-    testimSpeed = 4500,
-    currentSlide = 0,
-    currentActive = 0,
-    testimTimer,
-    touchStartPos,
-    touchEndPos,
-    touchPosDiff,
-    ignoreTouch = 30;
-;
-
-window.onload = function () {
-  // Testim Script
-  function playSlide(slide) {
-    for (var k = 0; k < testimDots.length; k++) {
-      testimContent[k].classList.remove("slide-active");
-      testimContent[k].classList.remove("slide-inactive");
-      testimDots[k].classList.remove("slide-active");
-    }
-
-    if (slide < 0) {
-      slide = currentSlide = testimContent.length - 1;
-    }
-
-    if (slide > testimContent.length - 1) {
-      slide = currentSlide = 0;
-    }
-
-    if (currentActive != currentSlide) {
-      testimContent[currentActive].classList.add("slide-inactive");
-    }
-
-    testimContent[slide].classList.add("slide-active");
-    testimDots[slide].classList.add("slide-active");
-    currentActive = currentSlide;
-    clearTimeout(testimTimer);
-    testimTimer = setTimeout(function () {
-      playSlide(currentSlide += 1);
-    }, testimSpeed);
-  }
-
-  testimLeftArrow.addEventListener("click", function () {
-    playSlide(currentSlide -= 1);
-  });
-  testimRightArrow.addEventListener("click", function () {
-    playSlide(currentSlide += 1);
-  });
-
-  for (var l = 0; l < testimDots.length; l++) {
-    testimDots[l].addEventListener("click", function () {
-      playSlide(currentSlide = testimDots.indexOf(this));
-    });
-  }
-
-  playSlide(currentSlide); // keyboard shortcuts
-
-  document.addEventListener("keyup", function (e) {
-    switch (e.keyCode) {
-      case 37:
-        testimLeftArrow.click();
-        break;
-
-      case 39:
-        testimRightArrow.click();
-        break;
-
-      case 39:
-        testimRightArrow.click();
-        break;
-
-      default:
-        break;
-    }
-  });
-  testim.addEventListener("touchstart", function (e) {
-    touchStartPos = e.changedTouches[0].clientX;
-  });
-  testim.addEventListener("touchend", function (e) {
-    touchEndPos = e.changedTouches[0].clientX;
-    touchPosDiff = touchStartPos - touchEndPos;
-    console.log(touchPosDiff);
-    console.log(touchStartPos);
-    console.log(touchEndPos);
-
-    if (touchPosDiff > 0 + ignoreTouch) {
-      testimLeftArrow.click();
-    } else if (touchPosDiff < 0 - ignoreTouch) {
-      testimRightArrow.click();
-    } else {
-      return;
-    }
-  });
-};
-},{}],"js/modal.js":[function(require,module,exports) {
-(function () {
-  var refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]')
-  };
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
-},{}],"js/modalhed.js":[function(require,module,exports) {
-(function () {
-  var refs = {
-    openModalBtn: document.querySelector('[data-modal-hed]'),
-    // closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]')
-  };
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
-},{}],"js/jquery.js":[function(require,module,exports) {
+})({"js/jquery.bpopup.min.js":[function(require,module,exports) {
 /*================================================================================
  * @name: bPopup - if you can't get it up, use bPopup
  * @author: (c)Bjoern Klinggaard (twitter@bklinggaard)
@@ -609,50 +397,7 @@ window.onload = function () {
     zIndex: 9997
   };
 })(jQuery);
-},{}],"js/main.js":[function(require,module,exports) {
-// Semicolon (;) to ensure closing of earlier scripting
-// Encapsulation
-// $ is assigned to jQuery
-;
-
-(function ($) {
-  // DOM Ready
-  $(function () {
-    var refs = {
-      closeModalBtn: document.querySelector('[data-modal-close]'),
-      modal: document.querySelector('[data-modal]')
-    }; // Binding a click event
-    // From jQuery v.1.7.0 use .on() instead of .bind()
-
-    $('#my-button').bind('click', function (e) {
-      // Prevents the default action to be triggered. 
-      e.preventDefault(); // Triggering bPopup when click event is fired
-
-      $('#element_to_pop_up').bPopup({
-        speed: 650,
-        transition: 'slideIn',
-        transitionClose: 'slideBack'
-      });
-    });
-  });
-})(jQuery);
-},{}],"index.js":[function(require,module,exports) {
-"use strict";
-
-require("./sass/main.scss");
-
-require("./js/burger");
-
-require("./js/slider");
-
-require("./js/modal");
-
-require("./js/modalhed");
-
-require("./js/jquery");
-
-require("./js/main");
-},{"./sass/main.scss":"sass/main.scss","./js/burger":"js/burger.js","./js/slider":"js/slider.js","./js/modal":"js/modal.js","./js/modalhed":"js/modalhed.js","./js/jquery":"js/jquery.js","./js/main":"js/main.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -680,7 +425,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52514" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49911" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -856,5 +601,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/src.e31bb0bc.js.map
+},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/jquery.bpopup.min.js"], null)
+//# sourceMappingURL=/jquery.bpopup.min.a836f41e.js.map
